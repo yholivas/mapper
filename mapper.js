ae6f354ddcf0e5ac93b28a83bc603371db7a5578
Daniel Olivas <yholivas@gmail.com>
2021-04-01 23:53:45 -0400

    beginnings of something

--- mapper.js
+++ mapper.js
@@ -1,14 +1,14 @@
-$( document ).ready(function() {
-    $( "a" ).click(function( event ) {
-        alert( "As you can see, the link no longer took you to jquery.com" );
-        event.preventDefault();
-    });
-});
+const loc = {x: 0, y: 0};
+var locations = [loc]
 
-$( "a" ).click(function( event ) {
- 
-    event.preventDefault();
- 
-    $( this ).hide( "slow" );
- 
-});
+function addLocation(x, y) {
+    let loc = {x: x, y: y};
+    locations.push(loc);
+    let printedList = "";
+    locations.forEach(
+        function(item, index, array) {
+            printedList += item.x.toString() + item.y.toString()
+        }
+    );
+    document.getElementById("locations").textContent = printedList;
+}
