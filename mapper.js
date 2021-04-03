9445bcc9d2dba276fc4c0c416ba368f7526964a3
Daniel Olivas <yholivas@gmail.com>
2021-04-02 22:07:15 -0400

    map image loads successfully

--- mapper.js
+++ mapper.js
@@ -1,5 +1,18 @@
 const loc = {x: 0, y: 0};
-var locations = [loc]
+var locations = [loc];
+
+var action;
+var reader = new FileReader();
+
+reader.onload = function(e) {
+    let ctx = document.getElementById('bbmap').getContext('2d');
+    ctx.mozImageSmoothingEnabled = false;
+    let img = new Image();
+    img.src = e.target.result;
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, 640, 400);
+    };
+};
 
 function addLocation(x, y) {
     let loc = {x: x, y: y};
@@ -11,4 +24,14 @@ function addLocation(x, y) {
         }
     );
     document.getElementById("locations").textContent = printedList;
+}
+
+function readImage(input) {
+    if (input.files && input.files[0]) {
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function setImage() {
+    readImage(document.querySelector('input[type="file"]'));
 }
