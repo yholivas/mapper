0ea0034d533f836f18a576983ad5b1900159c1cd
Daniel Olivas <yholivas@gmail.com>
2021-04-09 23:07:18 -0400

    added destroy triangle capability

--- mapper.js
+++ mapper.js
@@ -44,15 +44,15 @@ var tempMesh = new TriMesh();
 var locations = new Array();
 
 var reader = new FileReader();
+var ctx = document.getElementById('bbmap').getContext('2d');
+ctx.imageSmoothingEnabled = false;
+ctx.fillStyle = 'red';
 
 reader.onload = function(e) {
-    let ctx = document.getElementById('bbmap').getContext('2d');
-    ctx.imageSmoothingEnabled = false;
     let img = new Image();
     img.src = e.target.result;
     img.onload = function() {
         ctx.drawImage(img, 0, 0, 640, 400);
-        ctx.fillStyle = 'red';
     };
 };
 
@@ -62,7 +62,6 @@ function addLocation(e) {
     let loc = getOffsetLoc(e);
     locations.push(loc);
 
-    let ctx = document.getElementById('bbmap').getContext('2d');
     ctx.fillRect(loc.x - 3, loc.y, 4, 4);
 }
 
@@ -80,7 +79,6 @@ function removeLocation(e) {
     let idx = selectLocation(getOffsetLoc(e));
     if (idx != -1) {
         let loc = locations[idx];
-        let ctx = document.getElementById('bbmap').getContext('2d');
         locations.splice(idx, 1);
         ctx.clearRect(loc.x - 3, loc.y, 4, 4);
     }
@@ -88,7 +86,6 @@ function removeLocation(e) {
 
 function addLocToMesh(loc) {
     tempMesh.vertices.push(loc);
-    let ctx = document.getElementById('bbmap').getContext('2d');
     ctx.fillStyle = 'lime';
     ctx.fillRect(loc.x - 3, loc.y, 4, 4);
     ctx.fillStyle = 'red';
@@ -99,17 +96,61 @@ function constructTriMesh(e) {
     if (idx != -1) {
         var loc = locations[idx];
         addLocToMesh(loc);
-        if (tempMesh.vertices.length == 2) {
+        if (tempMesh.vertices.length == 1) {
+            ctx.beginPath();
+            ctx.moveTo(loc.x, loc.y);
+        } else if (tempMesh.vertices.length == 2) {
             // add new vertex to mesh, connect vertex in mesh to new vertex as neighbor
             graph.addVertex(tempMesh.vertices[0], loc);
+            ctx.lineTo(loc.x, loc.y);
         } else if (tempMesh.vertices.length == 3) {
             // add new vertex to mesh, connect vertices in mesh to new vertex, save mesh in map, create new temp mesh
             graph.addVertex(tempMesh.vertices[0], loc);
             graph.addVertex(tempMesh.vertices[1], loc);
             graph.meshes.push(tempMesh);
             tempMesh = new TriMesh();
+            ctx.lineTo(loc.x, loc.y);
+            ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+            ctx.fill();
         }
     }
+}
+
+function calc_area(v1, v2, v3)
+{
+    let det = 0;
+    det = (
+        (v1.x - v3.x) *
+        (v2.y - v3.y)) - (
+        (v2.x - v3.x) *
+        (v1.y - v3.y));
+    return Math.abs(det / 2);
+}
+
+function contains_point(mesh, loc)
+{
+    var vertices = mesh.vertices;
+    var total = calc_area(vertices[0], vertices[1], vertices[2]);
+    var area1 = calc_area(loc, vertices[1], vertices[2]);
+    var area2 = calc_area(loc, vertices[0], vertices[2]);
+    var area3 = calc_area(loc, vertices[0], vertices[1]);
+
+    if ((area1 + area2 + area3) > total)
+        return false;
+    else
+        return true;
+}
+
+function destroyTriMesh(e) {
+    let loc = getOffsetLoc(e);
+    // see if click is inside triangle
+    // if it is, delete that mesh
+    graph.meshes.forEach(function(itm, idx) {
+        if (contains_point(itm, loc)) {
+            graph.meshes.splice(idx, 1);
+            return;
+        }
+    });
 }
 
 function readImage(input) {
@@ -139,4 +180,9 @@ function setRemoveVertex(e) {
 function setCreateTriangle(e) {
     selectButton(e.target);
     document.getElementById('bbmap').onclick = constructTriMesh;
+}
+
+function setDestroyTriangle(e) {
+    selectButton(e.target);
+    document.getElementById('bbmap').onclick = destroyTriMesh;
 }
