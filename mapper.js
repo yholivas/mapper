fe3fab3dc08b1d886003be3ed6cd6776250693c0
Daniel Olivas <yholivas@gmail.com>
2021-04-10 19:15:23 -0400

    refactored all canvas code to its own function

--- mapper.js
+++ mapper.js
@@ -46,10 +46,11 @@ var locations = new Array();
 var reader = new FileReader();
 var ctx = document.getElementById('bbmap').getContext('2d');
 ctx.imageSmoothingEnabled = false;
-ctx.fillStyle = 'red';
 
+var canvasFunc;
+
+var img = new Image();
 reader.onload = function(e) {
-    let img = new Image();
     img.src = e.target.result;
     img.onload = function() {
         ctx.drawImage(img, 0, 0, 640, 400);
@@ -61,8 +62,6 @@ function getOffsetLoc(e) { return new Location(e.offsetX - 1, e.offsetY - 3); }
 function addLocation(e) {
     let loc = getOffsetLoc(e);
     locations.push(loc);
-
-    ctx.fillRect(loc.x - 3, loc.y, 4, 4);
 }
 
 function selectLocation(loc) {
@@ -80,44 +79,28 @@ function removeLocation(e) {
     if (idx != -1) {
         let loc = locations[idx];
         locations.splice(idx, 1);
-        ctx.clearRect(loc.x - 3, loc.y, 4, 4);
     }
-}
-
-function addLocToMesh(loc) {
-    tempMesh.vertices.push(loc);
-    ctx.fillStyle = 'lime';
-    ctx.fillRect(loc.x - 3, loc.y, 4, 4);
-    ctx.fillStyle = 'red';
 }
 
 function constructTriMesh(e) {
     let idx = selectLocation(getOffsetLoc(e));
     if (idx != -1) {
         var loc = locations[idx];
-        addLocToMesh(loc);
-        if (tempMesh.vertices.length == 1) {
-            ctx.beginPath();
-            ctx.moveTo(loc.x, loc.y);
-        } else if (tempMesh.vertices.length == 2) {
+        tempMesh.vertices.push(loc);
+        if (tempMesh.vertices.length == 2) {
             // add new vertex to mesh, connect vertex in mesh to new vertex as neighbor
             graph.addVertex(tempMesh.vertices[0], loc);
-            ctx.lineTo(loc.x, loc.y);
         } else if (tempMesh.vertices.length == 3) {
             // add new vertex to mesh, connect vertices in mesh to new vertex, save mesh in map, create new temp mesh
             graph.addVertex(tempMesh.vertices[0], loc);
             graph.addVertex(tempMesh.vertices[1], loc);
             graph.meshes.push(tempMesh);
             tempMesh = new TriMesh();
-            ctx.lineTo(loc.x, loc.y);
-            ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
-            ctx.fill();
         }
     }
 }
 
-function calc_area(v1, v2, v3)
-{
+function calc_area(v1, v2, v3) {
     let det = 0;
     det = (
         (v1.x - v3.x) *
@@ -127,18 +110,14 @@ function calc_area(v1, v2, v3)
     return Math.abs(det / 2);
 }
 
-function contains_point(mesh, loc)
-{
+function contains_point(mesh, loc) {
     var vertices = mesh.vertices;
     var total = calc_area(vertices[0], vertices[1], vertices[2]);
     var area1 = calc_area(loc, vertices[1], vertices[2]);
     var area2 = calc_area(loc, vertices[0], vertices[2]);
     var area3 = calc_area(loc, vertices[0], vertices[1]);
 
-    if ((area1 + area2 + area3) > total)
-        return false;
-    else
-        return true;
+    return (area1 + area2 + area3) <= total
 }
 
 function destroyTriMesh(e) {
@@ -161,6 +140,36 @@ function readImage(input) {
 
 function setImage() { readImage(document.querySelector('input[type="file"]')); }
 
+function updateCanvas(e) {
+    canvasFunc(e);
+    // clear everything
+    ctx.clearRect(0, 0, 640, 400);
+    // draw image
+    if (img) ctx.drawImage(img, 0, 0, 640, 400);
+    // draw locations array
+    ctx.fillStyle = 'red';
+    locations.forEach(function(itm) {
+        ctx.fillRect(itm.x - 3, itm.y, 4, 4);
+    });
+    // draw meshes
+    ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+    graph.meshes.forEach(function(itm) {
+        // first vertex
+        ctx.beginPath();
+        ctx.moveTo(itm.vertices[0].x, itm.vertices[0].y);
+        // second vertex
+        ctx.lineTo(itm.vertices[1].x, itm.vertices[1].y);
+        // third vertex
+        ctx.lineTo(itm.vertices[2].x, itm.vertices[2].y);
+        ctx.fill();
+    });
+    // probably draw any locations in tempmesh as green
+    ctx.fillStyle = 'lime';
+    tempMesh.vertices.forEach(function(itm) {
+        ctx.fillRect(itm.x - 3, itm.y, 4, 4);
+    });
+}
+
 function selectButton(btn) {
     if (document.getElementById('selected-btn'))
         document.getElementById('selected-btn').id = null;
@@ -169,20 +178,20 @@ function selectButton(btn) {
 
 function setAddVertex(e) {
     selectButton(e.target);
-    document.getElementById('bbmap').onclick = addLocation;
+    canvasFunc = addLocation;
 }
 
 function setRemoveVertex(e) {
     selectButton(e.target);
-    document.getElementById('bbmap').onclick = removeLocation;
+    canvasFunc = removeLocation;
 }
 
 function setCreateTriangle(e) {
     selectButton(e.target);
-    document.getElementById('bbmap').onclick = constructTriMesh;
+    canvasFunc = constructTriMesh;
 }
 
 function setDestroyTriangle(e) {
     selectButton(e.target);
-    document.getElementById('bbmap').onclick = destroyTriMesh;
+    canvasFunc = destroyTriMesh;
 }
